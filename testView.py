dd5044ee46b3ca206d72b45dcb1f2614ae40573a
Maria Todorova <m.todorova@blubito.com>
2023-05-03 11:29:21 +0300

    changed the filename of the unit test

--- testView.py
+++ testView.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import MagicMock
 from view import View
 
+
 class TestView(unittest.TestCase):
 
     def setUp(self):
@@ -13,12 +14,14 @@ class TestView(unittest.TestCase):
         self.assertEqual(button.cget("text"), "50$")
         self.assertEqual(button.cget("bg"), "purple")
         self.assertEqual(button.cget("fg"), "yellow")
+
     def test_check_answer(self):
         self.view.user_score = 0
         self.view.check_answer("Würzig", "Würzig", 100, self.view.user_score)
         self.assertEqual(self.view.user_score, 100)
         self.view.check_answer("False", "Würzig", 100, self.view.user_score)
         self.assertEqual(self.view.user_score, 100)
+
 
 if __name__ == '__main__':
     unittest.main()
